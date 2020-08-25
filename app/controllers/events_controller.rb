8e700d50cb35b2be44165943766d7cfbb273d692
Zeljka Vujic <vujic.zeljka@outlook.de>
2020-08-25 13:06:56 +0200

    Changes on the event show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,11 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show]
+  skip_before_action :authenticate_user!, only: [ :index, :show]
   def index
   end
 
   def show
+
   end
 
   def new
@@ -10,4 +13,16 @@ class EventsController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:date_time, :location, :title, :description)
+  end
+
+end
 end
