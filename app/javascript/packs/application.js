f2dc63afa5af0b3e00a4e95fbfb9827af096da53
Ekaterina Tugarinova <kate.tugarinova@gmail.com>
2020-08-25 16:28:01 +0200

    chatroom backend

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,7 +28,10 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
+import { initChatroomCable } from '../channels/chatroom_channel.js';
+
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initChatroomCable();
 });
