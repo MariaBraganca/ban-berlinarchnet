2ed3e4ab38ccd68bc3d6efaea6664bbbf8db77a2
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-08-25 21:47:59 +0200

    ore seeds

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
-  belongs_to :post, optional: true
-  belongs_to :event, optional: true
-  belongs_to :office, optional: true
+  belongs_to :post
+  belongs_to :event
+  belongs_to :office
   belongs_to :user
 
   validates :date, presence: true
