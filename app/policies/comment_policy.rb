4428b524837c8aaab8ac5096a685bc619f05bb39
Zeljka Vujic <vujic.zeljka@outlook.de>
2020-08-26 17:07:49 +0200

    Changes on the show

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -1,7 +1,21 @@
 class CommentPolicy < ApplicationPolicy
+  #before_action :set_comment, only: [:show, :destroy]
+  #skip_before_action :authenticate_user!, only: [ :index, :show]
   class Scope < Scope
     def resolve
       scope.all
     end
+
+
+    def create?
+      return true
+    end
+
+  end
+
+  private
+
+  def set_comment
+    @comment = Coment.find(params[:id])
   end
 end
--- app/policies/event_policy.rb
+++ app/policies/event_policy.rb
@@ -1,4 +1,5 @@
 class EventPolicy < ApplicationPolicy
+  
   class Scope < Scope
     def resolve
       scope.all
