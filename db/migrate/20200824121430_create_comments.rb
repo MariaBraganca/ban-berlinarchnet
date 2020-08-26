b5f995d97b9937f7aba4202aac3afdd893a076db
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-08-26 17:32:02 +0200

    sss

--- db/migrate/20200824121430_create_comments.rb
+++ db/migrate/20200824121430_create_comments.rb
@@ -3,9 +3,9 @@ class CreateComments < ActiveRecord::Migration[6.0]
     create_table :comments do |t|
       t.datetime :date
       t.text :content
-      t.references :post, foreign_key: true
-      t.references :event, foreign_key: true
-      t.references :office, foreign_key: true
+      t.references :post, null: true, foreign_key: true
+      t.references :event,null: true, foreign_key: true
+      t.references :office,null: true, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
