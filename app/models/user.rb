b323eb02f3a79ce6accdb55889958838fc104c45
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-08-26 16:56:54 +0200

    stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,14 @@
 class User < ApplicationRecord
   has_many :experiences
   has_many :offices, through: :experiences
-
   has_many :rsvps
   has_many :events, through: :rsvps
-
   has_many :messages
   has_many :chatrooms, through: :messages
-
   has_many :comments
-  
+
+  has_one_attached :photo
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
