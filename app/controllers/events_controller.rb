bca59ee8d03c4f3923c627a83d15a3df2819ba1b
Zeljka Vujic <vujic.zeljka@outlook.de>
2020-08-26 12:42:57 +0200

    Update, New and Delete function

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,20 +1,40 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show]
+  before_action :set_event, only: [:show, :destroy]
   skip_before_action :authenticate_user!, only: [ :index, :show]
   def index
   end
 
   def show
-    authorize @event
     @rsvp = Rsvp.new
+    authorize @event
   end
 
   def new
     @event = Event.new
     @user = current_user
+    authorize @event
   end
 
   def edit
+    @event = Event.find(params[:id])
+    @user = User.find(current_user.id)
+    authorize @event
+end
+
+  def destroy
+    @event.destroy
+    redirect_to events_path
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new
+    end
+    authorize @event
   end
 
   private
--- app/policies/event_policy.rb
+++ app/policies/event_policy.rb
@@ -8,6 +8,22 @@ class EventPolicy < ApplicationPolicy
   def show?
     true
   end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    record.user == user
+  end
   
+  def destroy?
+    record.user == user
+  end
+
 
 end
