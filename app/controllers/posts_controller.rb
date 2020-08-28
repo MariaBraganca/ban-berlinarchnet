7ecd1e2523c123ae3e7b68015a96b82f79e3fe3e
MariaBraganca <mcpbbraganca@gmail.com>
2020-08-28 13:24:22 +0200

    Hompage cards

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,13 @@
 class PostsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index, :show]
+
   def index
     @posts = policy_scope(Post).order(created_at: :desc)
   end
 
   def show
     @post = Post.find(params[:id])
+    authorize @post
   end
 
   def new
