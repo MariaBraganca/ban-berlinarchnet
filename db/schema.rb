69d9186148a8f6b7dec01e594d757ab409671b6a
MariaBraganca <mcpbbraganca@gmail.com>
2020-08-29 08:34:12 +0200

    Office Show Content

--- db/schema.rb
+++ db/schema.rb
@@ -39,10 +39,8 @@ ActiveRecord::Schema.define(version: 2020_08_28_123304) do
   create_table "chatrooms", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
     t.bigint "user_one_id"
     t.bigint "user_two_id"
-    t.index ["user_id"], name: "index_chatrooms_on_user_id"
     t.index ["user_one_id"], name: "index_chatrooms_on_user_one_id"
     t.index ["user_two_id"], name: "index_chatrooms_on_user_two_id"
   end
@@ -169,7 +167,6 @@ ActiveRecord::Schema.define(version: 2020_08_28_123304) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "chatrooms", "users"
   add_foreign_key "chatrooms", "users", column: "user_one_id"
   add_foreign_key "chatrooms", "users", column: "user_two_id"
   add_foreign_key "comments", "events"
