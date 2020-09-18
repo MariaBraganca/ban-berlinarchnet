f5fa4660a60bcf5f1d80178cabdcb830249a843f
Hristian Bozinoski <konten.bozinoski@gmail.com>
2020-09-18 20:20:38 +0200

    Incoroprated a date_time picker form npm

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,10 +34,3 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { initChatroomCable } from '../channels/chatroom_channel.js';
 
 import { initMapbox } from '../plugins/init_mapbox';
-
-document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-  initChatroomCable();
-  initMapbox();
-});
