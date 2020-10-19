5fc6b6ea7cdf093af9713ac1c64c5d41a852c2a6
MariaBraganca <mcpbbraganca@gmail.com>
2020-10-19 13:58:19 +0200

    updated event json file

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show]
 
   def index
-    @events = policy_scope(Event).order(created_at: :desc)
+    @events = policy_scope(Event).order(date: :desc)
     @user = User.find_by(email: @user)
   end
 
