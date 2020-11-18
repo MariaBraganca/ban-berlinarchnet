5880b3e7ea62d3ee70e23248fc2cccb01392d019
Maria Braganca <mcpbbraganca@gmail.com>
2020-11-18 16:20:00 +0100

    edited job_scraper and opening crud actions

--- app/models/opening.rb
+++ app/models/opening.rb
@@ -1,6 +1,8 @@
 class Opening < ApplicationRecord
   belongs_to :office, optional: true
 
+  has_rich_text :description
+
   validates :date, presence: true
   validates :job_position, presence: true
   validates :job_url, uniqueness: true
--- app/policies/opening_policy.rb
+++ app/policies/opening_policy.rb
@@ -8,4 +8,20 @@ class OpeningPolicy < ApplicationPolicy
   def show?
     true
   end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def edit?
+    true
+  end
+
+  def update?
+    true
+  end
 end
