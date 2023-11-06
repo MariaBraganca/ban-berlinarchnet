d02a48517f595bf0e5c4179d6ba85102c5df0c32
Maria Braganca <49592760+MariaBraganca@users.noreply.github.com>
2023-11-06 08:22:18 +0100

    remove rails test folder (#272)

--- test/controllers/office_projects_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class OfficeProjectsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get office_projects_index_url
-    assert_response :success
-  end
-
-end
